c649ab305c13621261f967d046a17995e48bc63b
Daniel Howard <danielhoward314@gmail.com>
2018-05-05 16:58:28 -0400

    is babel mad?

--- 001_knn/knn.js
+++ 001_knn/knn.js
@@ -1,8 +1,28 @@
 
 //Start off with what passes the first test.
-function KNN(kSize){
-	this.kSize = kSize;
-	this.points = [];
+class KNN {
+	constructor (kSize) {
+		this.kSize = kSize;
+		this.points = [];
+	}
+	train (dataArr) {
+		for (let i = 0; i < dataArr.length; i++){
+			this.points.push(dataArr[i]);
+		}
+	}
+	_distance = (vectOne, vectTwo) => {
+		let firstCalc = 0;
+		for (let i = 0; i < vectOne.length; i++){
+			firstCalc += ((Math.pow(vectOne[i] - vectTwo[i], 2)))
+		}
+		return Math.sqrt(firstCalc);
+	}
+	_distances = (vector, arr) => {
+		let output = [];
+		arr.forEach((element) => {
+			output.push([this._distance(vector, element[0]), element[1]]);
+		});
+	}
 }
 
 
